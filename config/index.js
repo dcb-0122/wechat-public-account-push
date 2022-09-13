68a9e455db99715c1a697c7781578f62eba814a6
dcb-0122 <40328904+dcb-0122@users.noreply.github.com>
2022-09-14 07:01:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ export const config = {
       // 想要发送的人的名字
       name: "小诗妍",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oTljm5rm00WXS_D3vPOEn8LwMqHE",
+      id: "oTljm5pTJD-UYR5ru63xodFvZLrk",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "36plnJtYhtQ6WfCZJlIhfm7qeYd2udnyz2ShWczZvTw",
       // 所在省份

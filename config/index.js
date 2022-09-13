fbe0c3600a5eaa29af4aa5d2b037a98496a835a1
dcb-0122 <40328904+dcb-0122@users.noreply.github.com>
2022-09-14 00:26:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oTljm5rm00WXS_D3vPOEn8LwMqHE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "y1pVUJazJFSF2nfaE5DDchPrygeB9XWIr6b9bIGSF1k",
+      useTemplateId: "5TiWYG6LHX4i82rmFKjRAgdXCmL_ZT_AJJOVH-H6RZM",
       // 所在省份
       province: "浙江",
       // 所在城市
@@ -37,7 +37,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "",
+      openUrl: "https://s.weibo.com/top/summary",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -130,9 +130,7 @@ export const config = {
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
       "少说狗，多说爱。",
-      "天气转冷，记得添衣。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
+      "天气转冷，记得添衣。"
     ]},
     // 你可以不断按格式往下增加
     // ...
